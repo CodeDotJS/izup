b26c3b47739c109129784bf329ec2010ee8958b2
Rishi Giri <rushforlinux@gmail.com>
2018-05-17 13:50:05 +0530

    tests for down

--- test.js
+++ test.js
@@ -1,8 +1,19 @@
 import childProcess from 'child_process';
 import test from 'ava';
 
-test.cb('default', t => {
+test.cb('up', t => {
 	const cp = childProcess.spawn('./cli.js', ['google.com'], {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
+		t.end();
+	});
+});
+
+test.cb('down', t => {
+	const cp = childProcess.spawn('./cli.js', ['rishigiri.com'], {stdio: 'inherit'});
 
 	cp.on('error', t.ifError);
 
